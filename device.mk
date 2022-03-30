45c9adb55a1c27211f6cf6d9a5c63b7ef04ce75c
UtsavBanar1231 <utsavbalar1231@gmail.com>
2022-03-29 21:06:20 -0300

    veux: Import camxoverride settings configuration

--- device.mk
+++ device.mk
@@ -115,7 +115,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
-    PRODUCT_PACKAGES += \
+# Camx overrides
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camxoverridesettings.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camxoverridesettings.txt
+
+PRODUCT_PACKAGES += \
     libpiex_shim
 
 # Component Overrides
