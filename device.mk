598a7cef4e46678cb6c6932b63ac82b12a63076d
SharmagRit <sharmagrit04@gmail.com>
2022-10-21 23:38:01 +0530

    veux: Import XiaomiParts *from - https://github.com/sweet-sm6150/device_xiaomi_sm6150-common.git
    Signed-off-by: SharmagRit <sharmagrit04@gmail.com>
    Signed off by: Monu <sunil9802506051@gmail.com>

--- device.mk
+++ device.mk
@@ -519,5 +519,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
 
+# XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Inherit the proprietary files
 include vendor/xiaomi/veux/veux-vendor.mk
