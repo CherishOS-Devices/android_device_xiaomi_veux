11d98971d87bf7ecfaee5c06aad1c22111cd2661
SharmagRit <sharmagrit04@gmail.com>
2022-10-26 19:23:52 +0530

    veux: Drop DeviceSettings -Remove DeviceSpecficSettings from system
    Signed off by: Monu <sunil9802506051@gmail.com>

--- device.mk
+++ device.mk
@@ -134,13 +134,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 
-# DeviceSettings
-PRODUCT_PACKAGES += \
-    DeviceSettings
-
-# Dex
-PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
